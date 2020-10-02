cb773f29f4a4158b113ce1aa641eaa46c02326bb
Riccardo Pasini <riccardo.pasini1992@gmail.com>
2020-10-02 15:41:51 +0200

    Inizio

--- index-maga.php
+++ index-maga.php
@@ -5,8 +5,16 @@ require_once "classes/Fumetto.php";
 $fumetto1 = new Fumetto("La profezia dell'armadillo", "Zerocalcare", "Edizioni Graficart");
 $fumetto1->prezzo = 15;
 var_dump($fumetto1);
-
-echo $fumetto1->prezzoTotale(4);
+echo $fumetto1->prezzoTotale(4)." euro";
 echo  "</br>" .$fumetto1->volumiTotali(4);
+
+$fumetto2 = new Fumetto("Tex", "Giovanni Luigi Bonelli & Aurelio Galleppini", "Sergio Bonelli Editore");
+$fumetto2->volumi = 110;
+$fumetto2->prezzo = 400;
+var_dump($fumetto2);
+echo $fumetto2->prezzoTotale(2)." euro";
+echo  "</br>" .$fumetto2->volumiTotali(2);
+
+
 
 ?>
